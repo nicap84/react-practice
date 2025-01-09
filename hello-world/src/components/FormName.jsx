5e0004deecf018ccf2a290c710f114b5465ff5cd
maria <maria.marin@appfire.com>
2025-01-09 17:29:15 +0100

    manage several inputs in a Form

--- hello-world/src/components/FormName.jsx
+++ hello-world/src/components/FormName.jsx
@@ -1,23 +1,65 @@
 import { useState } from "react";
 
 export default function FormName() {
-  const [name, setName] = useState("");
+  const [formData, setFormaData] = useState({
+    name: "",
+    age: "",
+    profession: "",
+  });
+
+  const [profiles, setProfiles] = useState([]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormaData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  }
 
   function handleEvent(event) {
     event.preventDefault();
-    console.log(name);
+    if (formData.name && formData.age && formData.profession) {
+      setProfiles([...profiles, formData]);
+      setFormaData({ name: "", age: "", profession: "" });
+    }
   }
 
   return (
     <div className="container mt-4">
       <form onSubmit={handleEvent}>
-        <input
-          className="form-control"
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-          placeholder="Enter your name"
-        />
+        <div className="form-group mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="form-group mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            placeholder="Enter age"
+          />
+        </div>
+        <div className="form-group mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="profession"
+            value={formData.profession}
+            onChange={handleChange}
+            placeholder="Enter profession"
+          />
+        </div>
         <button className="btn btn-primary mt-2">Submit</button>
       </form>
     </div>
